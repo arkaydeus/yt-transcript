5da1aa60dda3d0138e694feb66ade11ce61e0ffb
Ryu Kent <ryukent@gmail.com>
2022-04-21 11:10:32 +0900

    Implement transcription extraction

--- api/app.py
+++ api/app.py
@@ -1,19 +1,29 @@
-
-   
-from fastapi import FastAPI
 import datetime
+from urllib.parse import quote, unquote
+
+from fastapi import FastAPI
+from youtube_transcript_api import YouTubeTranscriptApi
+
+from api.yt_id import get_yt_id
 
 app = FastAPI()
 
-@app.get("/example/{parameter}")
-def example(parameter: str):
-    return {
-        "parameter": parameter,
-        "datetime": datetime.datetime.now().time()
-    }
 
-@app.get("/")
-def main():
-    return {
-        "message": "Hello my friend"
-    }
+@app.get("/decode/")
+def decode(url: str):
+
+    decoded: str = unquote(url)
+    yt_id: str = get_yt_id(decoded)
+    transcript_text = "Video not found"
+    if yt_id:
+        transcript: list = YouTubeTranscriptApi.get_transcript(yt_id)
+        if transcript and isinstance(transcript, list):
+            lines: list[str] = [str(x["text"]) for x in transcript]
+            transcript_text = "\n".join(lines)
+
+    return {"video_id": yt_id, "transcript": transcript_text}
+
+
+# @app.get("/")
+# def main():
+#     return {"message": "Path not found"}
